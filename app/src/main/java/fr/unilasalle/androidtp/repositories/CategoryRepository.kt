3fcc82f22d0c9555b5f5fd1cae015d9ced05105a
Theo Lomege <theolomege@gmail.com>
2023-12-12 23:44:49 +0100

    - Ajout de la méthode fetchCategories (API) & getAllCategories (BDD) dans le CategoryRepository.kt

--- app/src/main/java/fr/unilasalle/androidtp/repositories/CategoryRepository.kt
+++ app/src/main/java/fr/unilasalle/androidtp/repositories/CategoryRepository.kt
@@ -1,15 +1,37 @@
 package fr.unilasalle.androidtp.repositories
 
+import fr.unilasalle.androidtp.database.daos.CategoryDao
 import fr.unilasalle.androidtp.network.RetrofitService
 
 class CategoryRepository (
-    private val apiService : RetrofitService
+    private val apiService : RetrofitService,
+
+    private val categoryDao : CategoryDao
 ){
 
-    // Function to get all categories
+    /* Partie API */
+
+    /**
+     * Récupère la liste des catégories depuis l'API
+     */
     suspend fun fetchCategories(): List<String> {
         return apiService.getCategories()
     }
+
+    /* Partie BDD */
+
+    /**
+     * Récupère la liste des catégories depuis la BDD
+     */
+    fun getAllCategories(): List<String> {
+        return categoryDao.getAllCategories()
+    }
+
+
+
+
+
+
 
 
 }
